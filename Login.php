df294496e16a5e7d34fe8c16c4819e62bf51e2e5
Arjun7161 <akarjun7161@gmail.com>
2023-12-10 16:51:44 +0000

    css

--- Login.php
+++ Login.php
@@ -4,10 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login</title>
+    <link rel="stylesheet" href="stylelogin.css">
+
 </head>
 <body>
     <form action="">
         <h2>login</h2>
+        <Label>Name</Label>
+        <input type="text" name="loginname">
+        <label >Email</label>
+        <input type="text" name="loginemail">
+        <label >Password</label>
+        <input type="password" name="loginpassword">
+
+        <Button type="button">Login</Button>
     </form>
 </body>
 </html>
